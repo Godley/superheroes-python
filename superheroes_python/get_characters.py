37fda18e0e361ebfb1fb22651f35a22ef84b5de4
Charlotte Godley <charlotte@roseanddeer.com>
2024-02-23 11:59:10 +0000

    implement characters getting

--- superheroes_python/get_characters.py
+++ superheroes_python/get_characters.py
@@ -1,2 +1,18 @@
-def get_characters():
-    raise Exception("Not implemented")
+import requests
+import json
+from typing import Dict
+
+CHARACTERS_URL = "https://s3.eu-west-2.amazonaws.com/build-circle/characters.json"
+
+def get_characters() -> Dict:
+    """
+    1. Download from a given url
+    2. load it into a dictionary for now
+    3. return it
+    :return:
+    """
+    result = requests.get(CHARACTERS_URL)
+    if result.status_code != 200:
+        raise Exception(f"Status code was {result.status_code}")
+    loaded_result = json.loads(result.text)
+    return loaded_result
