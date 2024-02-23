f7214ff1a9d6d68b329ae10282bdf195851ae088
Charlotte Godley <charlotte@roseanddeer.com>
2024-02-23 14:11:41 +0000

    Add type annotations, implement edge cases
    - first check items is actually there
    - add character type checking
    - reduce hero score if against a weakness
    - check hero and villain are both found

--- superheroes_python/main.py
+++ superheroes_python/main.py
@@ -1,16 +1,29 @@
 from superheroes_python.get_characters import get_characters
+from typing import Dict
 
 
-def battle(hero_name, villain_name):
+def battle(hero_name: str, villain_name: str) -> Dict:
     characters = get_characters()
 
-    hero = None
-    villain = None
+    hero: Dict = None
+    villain: Dict = None
+
+    if characters.get("items") is None:
+        raise Exception("Invalid input structure")
 
     for character in characters["items"]:
-        if character["name"] == hero_name:
+        if character["name"] == hero_name and character["type"] == "hero":
             hero = character
-        if character["name"] == villain_name:
+        if character["name"] == villain_name and character["type"] == "villain":
             villain = character
 
+    if hero["weakness"] == villain_name:
+        hero["score"] -= 1
+
+    if hero is None or villain is None:
+        raise Exception("Hero or villain name not found in input structure")
+
     return hero if hero["score"] >= villain["score"] else villain
+
+if __name__ == "__main__":
+    print(battle("Batman", "Joker"))
